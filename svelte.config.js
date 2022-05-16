28c08e9faf534246b20f10e482ac8fc8a201bd34
Spiderlogical <27599581+xSpiderMC@users.noreply.github.com>
2022-05-16 16:24:19 -0700

    idk

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
+const dev = process.env.NODE_ENV === 'development';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,9 +15,9 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		prerender: {
-			enabled: true
-		}
+		// prerender: {
+		// 	enabled: true
+		// }
 	}
 };
 
